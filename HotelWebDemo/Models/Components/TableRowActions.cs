98d9bae0c7204fd63efddd6ee4d19ed119aed96e
Robert Sokolov <sokolov.r123@gmail.com>
2024-07-14 16:41:08 +0200

    Added table row actions

---
+++ HotelWebDemo/Models/Components/TableRowActions.cs
@@ -0,0 +1,23 @@
+﻿namespace HotelWebDemo.Models.Components;
+
+public class TableRowActions
+{
+    public string Controller { get; }
+
+    public int ItemId { get; }
+
+    public bool ConfirmDelete { get; }
+
+    public string ConfirmMessage { get; set; } = "Are you sure you want to delete this item?";
+
+    public string ConfirmModalId => $"delete{Controller}Modal-{ItemId}";
+
+    public string ConfirmModalLabelId => $"delete{Controller}ModalLabel-{ItemId}";
+
+    public TableRowActions(string controller, int itemId, bool confirmDelete = true)
+    {
+        Controller = controller;
+        ItemId = itemId;
+        ConfirmDelete = confirmDelete;
+    }
+}
